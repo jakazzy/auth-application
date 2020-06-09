c04983cecbbca539917fa62b12ef3624544a8dd3
jakazzy <jakazzy@gmail.com>
2020-06-09 02:04:13 +0000

    add user model

---
+++ user/src/model/user.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const User = mongoose.model('user', UserSchema)
+export default User 
--- user/src/v0/routers/v0.js
+++ user/src/v0/routers/v0.js
@@ -4,9 +4,9 @@ export default (express)=>{
 const router = express.Router()
 
 // authentication
-router.post('/login', v0.usersController.login)
+router.get('/login', v0.usersController.login)
 
-router.post('/signup', v0.usersController.create)
+router.get('/signup', v0.usersController.create)
 
 // 
 router.get('/users',  v0.usersController.index )
