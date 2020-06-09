282f3b5e0720a93db688dc85207ffc99ce721280
jakazzy <jakazzy@gmail.com>
2020-06-09 06:44:36 +0000

    set up project with and image of mongodb

--- user/src/server.js
+++ user/src/server.js
@@ -1,14 +1,29 @@
 import express from 'express'
 import Router from './v0/routers'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
 
 const app = express()
 const PORT = process.env.PORT || 8080
+
+app.use(bodyParser.urlencoded({ extended: false}))
+app.use('/api/v0', Router.v0Router(express))
+
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    'mongodb://mongo:27017/docker-node-mongo',
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 app.get('hello', (req, res)=>{
     res.send('hello how are you?')
 })
 
-app.use('/api/v0', Router.v0Router(express))
 app.listen(PORT, ()=>{
     console.log(`server running at http://localhost:${PORT}`)
     console.log(`press CTRL +C to stop server`)   
--- user/src/v0/controllers/usersController.js
+++ user/src/v0/controllers/usersController.js
@@ -1,10 +1,22 @@
+import Item from '../../model'
+
 export default {
     index: async(req,res)=>{ 
-        res.send('Hi this is working')
+        try {
+            const items = await Item.find()
+            res.status(200).send({message: items})
+        } catch (error) {
+            res.status(400).send({ message: error})
+        }
     },
 
     create: async(req, res)=>{
-        res.send("I see you created")
+     try {
+        res.send('Hi this is working')
+     } catch (error) {
+         res.status(400).send(error)
+     }
+     
     },
 
     login: async(req, res)=>{
